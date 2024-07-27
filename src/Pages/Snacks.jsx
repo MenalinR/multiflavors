036f218a7c4f952e2523c055e46ff47ab2445e0e
JOCKSHAN <jockshanak@gmail.com>
2024-07-27 12:54:06 +0530

    array got commented

--- src/Pages/Snacks.jsx
+++ src/Pages/Snacks.jsx
@@ -1,37 +1,37 @@
 import React from 'react'
 
-const SnacksData = [
-  {
-    id: 1,
-    img: snk1,
-    title:"snack"
+// const SnacksData = [
+//   {
+//     id: 1,
+//     img: snk1,
+//     title:"snack"
 
-  },
-  {
-    id: 2,
-    img: snk1,
-    title:"snack"
+//   },
+//   {
+//     id: 2,
+//     img: snk1,
+//     title:"snack"
 
-  },
-  {
-    id: 3,
-    img: snk1,
-    title:"snack"
+//   },
+//   {
+//     id: 3,
+//     img: snk1,
+//     title:"snack"
 
-  },
-  {
-    id: 4,
-    img: snk1,
-    title:"snack"
+//   },
+//   {
+//     id: 4,
+//     img: snk1,
+//     title:"snack"
 
-  },
-  {
-    id: 5,
-    img: snk1,
-    title:"snack"
+//   },
+//   {
+//     id: 5,
+//     img: snk1,
+//     title:"snack"
 
-  },
-]
+//   },
+// ]
 const Snacks = () => {
   return (
     <div className='mt-14 mb-12'>
