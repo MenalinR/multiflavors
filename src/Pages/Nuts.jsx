0c12859407ad6fd73c2a24cff14903576de6a308
Menalin <menalin4@gmail.com>
2024-07-29 22:55:48 +0530

    added nuts images

--- src/Pages/Nuts.jsx
+++ src/Pages/Nuts.jsx
@@ -1,4 +1,47 @@
 import React from 'react'
+import nut1 from "../assets/Nuts/nut1.jpg"
+import nut2 from "../assets/Nuts/nut2.jpg"
+import nut3 from "../assets/Nuts/nut3.jpg"
+import nut4 from "../assets/Nuts/nut4.jpg"
+import nut5 from "../assets/Nuts/nut5.jpg"
+
+const NutsData = [
+  {
+      id: 1,
+      img: nut1,
+      title:"Cashews",
+      
+  
+    },
+    {
+      id: 2,
+      img: nut2,
+      title:"Peanuts",
+      
+  
+    },
+    {
+      id: 3,
+      img: nut3,
+      title:"Pistachios",
+      
+  
+    },
+    {
+      id: 4,
+      img: nut4,
+      title:"Almonds",
+      
+  
+    },
+    {
+      id: 5,
+      img: nut5,
+      title:"Walnuts",
+      
+  
+    },
+]
 
 const Nuts = () => {
   return (
@@ -8,10 +51,30 @@ const Nuts = () => {
         <h1 className='text-4xl font-bold font-serif'>Nuts</h1>
       </div>
       <div>
-        <div>
-          {
+        <div className='grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 place-items-center gap-8'>
+        {
+                NutsData.map((data) => (
 
-          }
+                    <div className='space-y-3'>
+                        <img
+                        src={data.img}
+                        className='h-[220px] w-[200px] object-cover rounded-md' />
+
+                        <div className='text-center'>
+                            <h3 className='font-semibold'>{data.title}</h3>
+                            <div className='pt-4'>
+                            <button className='bg-gradient-to-r from-black to-black hover:scale-105 duration-200 text-white py-2 px-4 rounded-full font-bold'>
+                            Select Options
+
+                    </button>
+                            </div>
+
+                        </div>
+
+                    </div>
+                ))
+ 
+            }
 
         </div>
       </div>
