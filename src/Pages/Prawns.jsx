e69b2af5448ef3ceae9176b3caf55bff03f24d50
Menalin <menalin4@gmail.com>
2024-07-30 10:21:05 +0530

    added prawns images

--- src/Pages/Prawns.jsx
+++ src/Pages/Prawns.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Prowns from '../assets/Prawns.jpeg'
 
 const Prawns = () => {
   return (
@@ -8,10 +9,20 @@ const Prawns = () => {
           <h1 className='text-4xl font-bold font-serif'>Prawns</h1>
         </div>
         <div>
-          <div>
-            {
+          <div className='flex items-center space-x-10'>
+            
+<img class="h-auto max-w-xl rounded-lg shadow-xl dark:shadow-gray-800" src={Prowns} alt="image description" />
+<div className='pl-10'>
+<div className='pl-5'>
+<h3>Fried Prawns</h3>
 
-            }
+</div>
+<div className='py-5'>
+<button className='bg-gradient-to-r from-black to-black hover:scale-105 duration-200 text-white py-2 px-4 rounded-full font-bold'>
+Select Options
+</button></div>
+</div>
+
 
           </div>
         </div>
