0150e58fae17fa23ac6ca11fd6d76ace9391c2c6
Menalin <menalin4@gmail.com>
2024-08-15 10:33:30 +0530

    updated testimonials

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,38 +1,36 @@
 import React from 'react';
 import Slider from 'react-slick';
-
+import img1 from '../../assets/Testi/dars.jpg'
+import img2 from '../../assets/Testi/seles.jpg'
+import img3 from '../../assets/Testi/vash.jpg'
+import img4 from '../../assets/Testi/mena.jpg'
 
 const TestimonialData =[
     {
         id: 1,
-        name: "Maria S.",
-        text: "High-quality and delicious. The dry fish adds an amazing depth of flavor to my dishes. Highly recommend for any seafood lover!",
-        img: "https://www.perfocal.com/blog/content/images/size/w960/2021/01/Perfocal_17-11-2019_TYWFAQ_100_standard-3.jpg",
+        name: "Dharshini Croos",
+        text: "Excellent products from Mannar by a young Engineer in partnership with an IT expert. Genuine home made products made with love.",
+        img: img1,
     },
     {
         id: 2,
-        name: "Linda B.",
-        text: "Incredible flavors and textures! The sweets are heavenly, and the snacks are always fresh and satisfying. I keep coming back for more.",
-        img: "https://expertphotography.b-cdn.net/wp-content/uploads/2018/10/mike-fox-467499-unsplash.jpg",
+        name: "Selestina",
+        text: "Yummiest homemade food in town 😍😍",
+        img: img2
     },
     {
         id: 3,
-        name: "Jason M.",
-        text: "These sweets and snacks are simply the best! The variety is fantastic, and everything tastes amazing. Highly recommend!",
-        img: "https://media.istockphoto.com/id/1347005975/photo/portrait-of-a-serious-muslim-young-man-looking-at-camera.jpg?s=612x612&w=0&k=20&c=mxRUDCuwDD3ML6-vMaUlTY7Ghqlj2R_LOhWWCB5CTXE=",
+        name: "Vashni",
+        text: "Premium Quality and Ready to cook Dry fish. Must try 🤤",
+        img: img3
     },
     {
         id: 4,
-        name: "Hannah W.",
-        text: "Exceptional quality! The dry fish is fresh and flavorful. It has elevated my cooking to a whole new level.",
-        img: "https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/profile-photos-2.jpg",
+        name: "Menalin",
+        text: "Wow. Delicious snacks and sweets😋",
+        img: img4
     },
-    {
-        id: 5,
-        name: " Emily J.",
-        text: "These peanuts are the best snack ever! Crunchy, fresh, and perfectly roasted. I can't stop munching on them.",
-        img: "https://i.pinimg.com/originals/d7/ac/8f/d7ac8f7ec220e555878261e1957dfb83.jpg",
-    },
+    
 ]
 const Testimonials = () => {
 
@@ -90,29 +88,32 @@ const Testimonials = () => {
                     TestimonialData.map((data) => (
                         <div className='my-6'>
                         <div
-                        key={data.id}
-                        className='flex flex-col gap-4 shadow-lg py-B px-6 mx-4 rounded-xl'
-                        >
-                            <div className='mb-4'>
-                                <img
-                                src={data.img}
-                                className='rounded-full w-20 h-20'
-                                />
+    key={data.id}
+    className='flex flex-col gap-4 shadow-lg py-4 px-6 mx-4 rounded-xl h-[200px] w-[500px] relative'
+>
+    <div className='mb-4 flex right-0'>
+        <img
+            src={data.img}
+            className='rounded-full w-20 h-20'
+        />
+    </div>
+    <div className='flex flex-col items-center justify-between flex-1'>
+        <div className='text-center'>
+            <p className='text-sm text-gray-500'>
+                {data.text}
+            </p>
+        </div>
+        <div className='text-center'>
+            <h1 className='text-xl font-bold text-black/80 dark:text-light'>
+                {data.name}
+            </h1>
+        </div>
+    </div>
+    <p className='text-black/20 text-9xl font-serif absolute top-0 right-5'>
+        ''
+    </p>
+</div>
 
-                            </div>
-                            <div className='flex flex-col items-center gap-4'>
-                                <div className='space-y-3'>
-                                <p className='text-sm text-gray-500'>
-                                    {data.text}
-                                </p>
-                                <h1 className='text-xl font-bold text-black/80 dark:text-light'>{data.name}</h1>
-                                </div>
-                            </div>
-                            <p className='text-black/20 text-9xl font-serif absolute top-0 right-0'> 
-                                ''
-                            </p>
-
-                        </div>
                         </div>
                     ))
                     }
