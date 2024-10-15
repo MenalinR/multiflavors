3b3979b56b2a16eab445951f39390ea4f77742f6
Menalin <menalin4@gmail.com>
2024-10-15 20:09:05 +0530

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/"
-})
+  base: "/", // Ensure the base path is correct for GitHub Pages
+  build: {
+    rollupOptions: {
+      external: ['firebase/firestore'], // Externalize Firebase Firestore
+    },
+  },
+});
