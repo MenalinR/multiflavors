ac3ce4aa5731b49db496eb8fe8617c2027fdfe59
Menalin <menalin4@gmail.com>
2024-10-17 23:37:01 +0530

    secrets got saved in gh

--- src/firebase.js
+++ src/firebase.js
@@ -2,25 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APPID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID,
+};
 
-    apiKey: "AIzaSyBuPpjfEvrCOU_w2MTwgZKcfj4zWajhD40",
-  
-    authDomain: "e-commerce-5db87.firebaseapp.com",
-  
-    projectId: "e-commerce-5db87",
-  
-    storageBucket: "e-commerce-5db87.appspot.com",
-  
-    messagingSenderId: "377064810828",
-  
-    appId: "1:377064810828:web:da36cbfbefa48d951f07e9",
-  
-    measurementId: "G-B2B12CBWFS"
-  
-  };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 // Initialize Firestore
-export const db = getFirestore(app); 
-
+export const db = getFirestore(app);
